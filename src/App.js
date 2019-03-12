8dd4e3fc62b9260968f5a3d2807a3d6750d88392
Kevin Edmondson <kevinraye13@gmail.com>
2019-03-12 11:03:09 -0500

    autoprefixer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./build/styles.css";
+// import "./styles.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Post Css</h1>
+        <div className="cont">
+          <div className="box">one</div>
+          <div className="box">two</div>
+          <div className="box">three</div>
+          <div className="box">four</div>
+        </div>
+        <button className="btn">This is a button</button>
       </div>
     );
   }
